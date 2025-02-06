be2e076f141573c7a27cfa895f1c1f9e5f852895
beatricexc <72341578+beatricexc@users.noreply.github.com>
2025-02-06 18:32:03 +0000

    add data validation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,16 +1,23 @@
 namespace EmployeeManagement.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class Employee
 {
+    [Required]
     public int Id { get; set; }
     
+    [Required]
     public string FirstName { get; set; }
     
+    [Required]
     public string LastName { get; set; }
     
+    [Required]
     public string Email { get; set; }
     
+    [Required]
     public string Phone { get; set; }
     
+    [Required]
     public string Position { get; set; }
 }
